ffecc66a8c3d57d2042b65cde88d8c04bd5bb0d3
Ivan <pv27643@alunos.estgv.ipv.pt>
2026-02-08 19:34:49 +0000

    ficheiros removidos

--- mobile/lib/models/patient.dart
+++
@@ -1,7 +0,0 @@
-class Patient {
-  final String id;
-  final String name;
-  final String type;
-
-  const Patient({required this.id, required this.name, required this.type});
-}
--- mobile/lib/pages/dependents_page.dart
+++
@@ -1,2 +0,0 @@
-// Forwarding file to preserve legacy import paths.
-export '../dependents_page.dart';
